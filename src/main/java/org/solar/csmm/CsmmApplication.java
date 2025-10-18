7a8850829262620ffcbb4fda8073f092f05b20f2
solarboro <boty.dh@qq.com>
2025-10-18 15:52:37 +0800

    init

---
+++ src/main/java/org/solar/csmm/CsmmApplication.java
@@ -0,0 +1,13 @@
+package org.solar.csmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/solar/csmm/CsmmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.solar.csmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
